ccb0cfe9bb9b698b62656d541bf985efbd27cfc3
drexelcybercamp <86355978+drexelcybercamp@users.noreply.github.com>
2024-12-21 21:34:02 -0500

    timer fix

--- src/utils/timer.js
+++ src/utils/timer.js
@@ -1,16 +1,15 @@
+const timerElement = document.querySelector('#timer');
+if (timerElement) {
+  timerElement.addEventListener('click', startTimer);
+}
+
 export const startTimer = (callback, duration) => {
-    let timer = duration;
-    const interval = setInterval(() => {
-      timer -= 1;
-      if (timer <= 0) {
-        clearInterval(interval);
-        if (callback) callback();
-      }
-    }, 1000);
-  };
-  
-  const timerElement = document.querySelector('#timer');
-  if (timerElement) {
-    timerElement.addEventListener('click', () => startTimer(null, 60));  // Default 60 seconds for demo
-  };
-  
+  let timer = duration;
+  const interval = setInterval(() => {
+    timer -= 1;
+    if (timer <= 0) {
+      clearInterval(interval);
+      if (callback) callback();
+    }
+  }, 1000);
+};
